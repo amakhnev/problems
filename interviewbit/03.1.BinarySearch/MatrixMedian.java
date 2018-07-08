2319153459aa15047e390068cd7d7bc4d0a6e860
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-08 14:59:59 +0100

    Binary search solutions

---
+++ interviewbit/03.1.BinarySearch/MatrixMedian.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MatrixMedian {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
+
+        input.add(new ArrayList<>(Arrays.asList(1,3,5)));
+        input.add(new ArrayList<>(Arrays.asList(2,6,9)));
+        input.add(new ArrayList<>(Arrays.asList(3,6,9)));
+
+        System.out.println("Result = " + new MatrixMedian().findMedian(input));
+    }
+
+
+    public int findMedian(ArrayList<ArrayList<Integer>> A) {
+
+        int min = A.get(0).get(0), max = A.get(0).get(0);
+        int n = A.size(), m = A.get(0).size();
+        for (int i = 0; i < n; ++i) {
+            if (A.get(i).get(0) < min) min = A.get(i).get(0);
+            if (A.get(i).get(m-1) > max) max = A.get(i).get(m-1);
+        }
+
+        int element = (n * m + 1) / 2;
+        while (min < max) {
+            int mid = min + (max - min) / 2;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i)
+                cnt += numOfLessOrEqual(A.get(i),mid);
+            if (cnt < element)
+                min = mid + 1;
+            else
+                max = mid;
+        }
+        return min;
+
+    }
+
+    public int numOfLessOrEqual(ArrayList<Integer> A,int B){
+        int left =0;int right = A.size();
+
+        while (left<right){
+            int mid = (left + right )/ 2;
+            if (A.get(mid)>B){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
---
+++ interviewbit/03.1.BinarySearch/MatrixSearch.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MatrixSearch {
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> input = new ArrayList<>();
+        input.add(new ArrayList<>(Arrays.asList(2, 9, 12, 13, 16, 18, 18, 19, 20, 22)));
+        input.add(new ArrayList<>(Arrays.asList(29, 59, 62, 66, 71, 75, 77, 79, 97, 99)));
+
+        System.out.println("result = " + new MatrixSearch().searchMatrix(input,23));
+    }
+
+    public int getValue(ArrayList<ArrayList<Integer>> a, int k, int m){
+        return a.get(k/m).get(k%m);
+    }
+
+    public int searchMatrix(ArrayList<ArrayList<Integer>> a, int b) {
+        int n = a.size(); int m = a.get(0).size();
+
+        int min = 0, max = n*m-1;
+
+        if (getValue(a,min,m) > b || getValue(a,max,m) < b){
+            return 0;
+        }
+
+        while (min<=max){
+            int mid = min + (max-min) / 2;
+            int midvalue = getValue(a,mid,m);
+            if (midvalue==b){
+                return 1;
+            }
+            if (midvalue<b){
+                min = mid+1;
+            }else{
+                max= mid-1;
+            }
+        }
+
+        return 0;
+    }
+}
