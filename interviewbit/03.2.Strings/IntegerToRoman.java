a1b03e4c537ac5702cc770ad43f74512727490ec
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-14 17:20:19 +0100

    Strings solutions

---
+++ interviewbit/03.2.Strings/IntegerToRoman.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Stack;
+
+public class IntegerToRoman {
+
+
+    public static void main(String[] args) {
+        System.out.println("Result = " + new IntegerToRoman().intToRoman(19));
+    }
+
+    public String intToRoman(int A) {
+        StringBuilder sb = new StringBuilder();
+
+
+        int [] numbers = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String [] letters = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i=0;i<numbers.length;i++){
+            while (A / numbers[i] >0){
+                sb.append(letters[i]);
+                A -=numbers[i];
+            }
+        }
+
+
+        return sb.toString();
+
+    }
+
+}
---
+++ interviewbit/03.2.Strings/LengthOfLastWord.java
@@ -0,0 +1,23 @@
+public class LengthOfLastWord {
+
+    public static void main(String[] args) {
+        System.out.println("Result = " + new LengthOfLastWord().lengthOfLastWord(" world  s"));
+    }
+
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int lengthOfLastWord(final String A) {
+        int prev =0, current =0, idx=0;
+        while (idx<A.length()){
+            if (A.charAt(idx) == ' '){
+                if (current>0){
+                    prev = current;
+                }
+                current=0;
+            }else{
+                current++;
+            }
+            idx++;
+        }
+        return current>0?current:prev;
+    }
+}
