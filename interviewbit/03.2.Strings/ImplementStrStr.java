2443e0c4f85108867e5930ad093f910d192db7a0
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-16 14:32:25 +0100

    Strings solutions

---
+++ interviewbit/03.2.Strings/ImplementStrStr.java
@@ -0,0 +1,24 @@
+public class ImplementStrStr {
+
+
+    public static void main(String[] args) {
+        System.out.println("Result  = " + new ImplementStrStr().strStr("baba","b"));
+    }
+
+
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int strStr(final String A, final String B) {
+        if (B.length()==0|| A.length()<B.length()){
+            return -1;
+        }
+
+        for (int i=0; i<= A.length() - B.length();i++){
+            if (A.substring(i,i+B.length()).equals(B)){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+}
