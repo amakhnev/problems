60c9d14740d04d9e4093bf57d09c0b5c964aa7fb
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-20 09:19:17 +0100

    Bit manipulation solutions

---
+++ interviewbit/03.3.BitManipulation/ReverseBits.java
@@ -0,0 +1,18 @@
+public class ReverseBits {
+    public static void main(String[] args) {
+        System.out.println("Result = " + new ReverseBits().reverse(1));
+    }
+
+
+
+    public long reverse(long a) {
+        long result = a & 1;
+        for (int i = 0; i < 31; i++) {
+            result <<= 1;
+            a >>>= 1;
+            result |= a & 1;
+        }
+        return result;
+    }
+
+}
