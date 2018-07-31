ffadfc03fd4c2a69cf7b7c2656b6fc35e4825981
Alexey Makhnev <alexey@integrace.co.uk>
2018-07-31 11:10:37 +0100

    Linked lists solutions

---
+++ interviewbit/04.1.LinkedLists/PartitionList.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class PartitionList {
+
+    public static void main(String[] args) {
+
+        System.out.println("Result = " + new PartitionList().partition(
+                ListNode.create(Arrays.asList(1, 12, 8, 4, 5, 7, 4, 1, 9, 2, 11, 12)), 4
+        ));
+    }
+
+
+    public ListNode partition(ListNode A, int B) {
+        ListNode rightNodesHeader = new ListNode(0),leftNodesHeader = new ListNode(0),iterator = A,
+                lastRight = rightNodesHeader,lastLeft = leftNodesHeader;
+        while (iterator!=null){
+            ListNode next =iterator.next;
+            if (iterator.val>=B){
+                lastRight.next=iterator;
+                iterator.next=null;
+                lastRight=iterator;
+            }else{
+                lastLeft.next=iterator;
+                iterator.next=null;
+                lastLeft = iterator;
+            }
+            iterator = next;
+        }
+        lastLeft.next=rightNodesHeader.next;
+        return leftNodesHeader.next;
+    }
+}
