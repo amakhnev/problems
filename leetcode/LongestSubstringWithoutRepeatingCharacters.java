50fc3286819843c7094a764d233ff1dbcfaa4043
Alexey Makhnev <alexey@integrace.co.uk>
2018-08-01 22:21:21 +0100

    leetcode solutions

---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        System.out.println("Result = "+new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring("safdsgfghghkjhkhgjkwertfghj"));
+    }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> chars = new HashSet<>();
+        int i=0,j=0,maxLen=0;
+        while(j<s.length()&&i<s.length()){
+            if (!chars.contains(s.charAt(j))){
+                chars.add(s.charAt(j++));
+                maxLen = Math.max(maxLen,j-i);
+            }else {
+                chars.remove(s.charAt(i++));
+            }
+        }
+        return maxLen;
+    }
+
+}
